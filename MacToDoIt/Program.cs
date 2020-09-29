0325ee21803611928ea901413ce52707d18a2ade
Ulf Bengtsson <ulf.bengtsson@lexicon.se>
2020-09-29 15:51:09 +0200

    Add project files.

---
+++ MacToDoIt/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MacToDoIt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
